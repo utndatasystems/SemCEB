543f7ba7560f66ae4457908175ec6bc1b39c9aa1
Claudius <152037418+KClaudius@users.noreply.github.com>
2026-05-11 22:20:53 +0200

    setup data loader for csv

--- data/loader.py
+++ data/loader.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 import pandas as pd
 
 
@@ -5,8 +7,19 @@ class DataLoader:
     """Loads locally stored data."""
 
     def __init__(self):
-        self.folderpath_raw_data = r"data\.raw"
+        self.folderpath_raw_data = Path(r"data\.raw")
 
     def load(self, dataset: str, scale_factor: int) -> pd.DataFrame:
-        """Load raw data into pandas dataframe"""
-        return pd.DataFrame()
+        """Load raw data into pandas dataframe."""
+
+        filepath = (
+            self.folderpath_raw_data
+            / dataset
+            / f"scale_factor_{scale_factor}"
+            / "table.csv"
+        )
+
+        if not filepath.exists():
+            raise FileNotFoundError(f"Dataset file not found: {filepath}")
+
+        return pd.read_csv(filepath)

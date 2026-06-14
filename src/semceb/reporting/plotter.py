222e6d63d8537dfeb69aaccd7e8b5930b536187e
Andi Zimmerer <andreas.zimmerer@utn.de>
2026-06-14 09:28:39 +0200

    factor out apply_plot_params()

--- src/semceb/reporting/plotter.py
+++ src/semceb/reporting/plotter.py
@@ -9,38 +9,7 @@ import pandas as pd
 from semceb.utils.console import console
 from rich.table import Table
 import seaborn as sns
-
-
-def apply_plot_params(
-    fig_height: float,
-    scale: float = 1.0,
-    double_column: bool = False,
-) -> None:
-    mpl.rcParams.update(
-        {
-            "figure.figsize": (
-                (7.00697 if double_column else 3.3374) * scale,
-                fig_height * scale,
-            ),
-            "figure.dpi": 300,
-            "font.size": 11.0,
-            "font.family": "serif",
-            "axes.titlesize": "medium",
-            "axes.labelsize": "medium",
-            "figure.titlesize": "medium",
-            "xtick.labelsize": "medium",
-            "ytick.labelsize": "medium",
-            "legend.fontsize": "medium",
-            "legend.title_fontsize": "medium",
-            "text.usetex": True,
-            "text.latex.preamble": (
-                r"\usepackage{amsmath}\usepackage{amssymb}"
-                r"\usepackage{siunitx}[=v2]"
-            ),
-            "pgf.rcfonts": False,
-            "pgf.texsystem": "pdflatex",
-        }
-    )
+from .plot_params import apply_plot_params
 
 
 class ResultsPlotter:
